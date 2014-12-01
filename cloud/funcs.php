eb16f3f8c22c432caa7a24a45f28a8389adc40ba
JoshDreamland <JoshV@zoominternet.net>
2014-11-30 21:43:48 -0700

    Adds an upload function to the file manager.

--- cloud/funcs.php
+++ cloud/funcs.php
@@ -78,8 +78,23 @@ function getObjects($store, $container, $prefix = null, $authHdr) {
 	return json_decode($output);
 }
 
-function getObjectsByPath($store, $container, $path, $authHdr) {
+function getObjectsByPath($store, $container, $path, $authHdr, $limits = FALSE) {
 	$params = array('format' => 'json', "prefix" => $path, "delimiter" => "/");
+	if ($limits !== FALSE) {
+		foreach ($limits as $kind => $limit) {
+			switch ($kind) {
+				case "limit": case "count":
+					$params["limit"] = intval($limit);
+					break;
+				case "upper": case "before": case "marker_end":
+					$params["marker_end"] = $limit;
+					break;
+				case "lower": case "after": case "marker":
+					$params["marker"] = $limit;
+					break;
+			}
+		}
+	}
 	$url = getUrl($store, $container, null, $params);
 	$output = curl($url, $authHdr);
 	return json_decode($output);
@@ -102,6 +117,95 @@ function copyObjectToContainer($store, $container, $obj, $newContainer, $newObj,
 function deleteObject($store, $container, $obj, $authHdr) {
 	$url = getUrl($store, $container, $obj);
 	return curl($url, $authHdr, null, 'DELETE', array('CODE'));
+}
+
+function oldphp_curl_file_create($filename, $mimetype, $postname) {
+  return "@$filename;filename=$postname;type=$mimetype";
+}
+
+function parse_headers($header) {
+	$arr = preg_split('/\s*([A-Za-z0-9\-]+)\s*:\s*(.*?)\s*(?=\n)/', $header, -1, PREG_SPLIT_DELIM_CAPTURE);
+	$num = count($arr) - 1;
+	$result	= Array();
+	for ($i	= 1; $i < $num;	$i += 3) {
+		$result[strtolower($arr[$i])] = $arr[$i+1];
+	}
+	return $result;
+}
+
+/**
+ * Uploads the given file to the given path.
+ * @return TRUE if the upload succeeded; otherwise, the error text
+ */
+function putObject($store, $container, $destpath, $file, $authHdr) {
+	$params =  array('format' => 'json');
+	$ch = curl_init(getUrl($store, $container, $destpath, $params));
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $authHdr);
+	if (isset($post) && !empty($post))
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	if (isset($method) && !empty($method))
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+
+	$fname = isset($file['tmp_name'])? $file['tmp_name'] : $file['name'];
+	$mtype = isset($file['mime'])? $file['mime']
+			: isset($file['mimetype'])? $file['mimetype']
+			: $file['type'];
+	$size = isset($file['size'])? $file['size'] : $file['filesize'];
+
+	$fstream = fopen($fname, 'rb');
+	if ($fstream === FALSE) {
+		 return array(
+                        'success' => FALSE,
+                        'code'    => 404,
+                        'error'   => 'The file you specified could not be opened.');
+	}
+
+	curl_setopt($ch, CURLOPT_PUT, TRUE);
+	curl_setopt($ch, CURLOPT_INFILE, $fstream);
+	curl_setopt($ch, CURLOPT_INFILESIZE, $size);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_VERBOSE, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+
+	$response = curl_exec($ch);
+
+	$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+	$header = substr($response, 0, $header_size);
+	$output = substr($response, $header_size);
+
+	$headerdata = parse_headers($header);
+	// echo '<pre>headers: '; print_r($header); echo '</pre>';
+	// echo '<pre>headerdata: '; print_r($headerdata); echo '</pre>';
+
+	if (curl_errno($ch) != 0) {
+		$error = curl_error($ch);
+		curl_close($ch);
+		return array(
+			'success' => FALSE,
+			'code'    => $status,
+			'error'   => $error);
+	}
+
+	$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if ($status != 201) {
+		return  array(
+                        'success' => FALSE,
+                        'code'    => $status,
+                        'error'   => $output);
+	}
+	$info = json_decode($output);
+	// echo "<pre>dump: `"; print_r($info); echo '`</pre>';
+	// echo '<pre>Output: '; print_r($output); echo '</pre>';
+	return array(
+		'success' => TRUE,
+		'code'    => $status,
+		'type'    => $headerdata['content-type'],
+		'hash'    => $headerdata['etag'],
+		'date'    => $headerdata['date']);
 }
 
 function getHdrs($store, $container, $obj, $authHdr) {
--- list-files.php
+++ list-files.php
@@ -1,3 +1,4 @@
+
 <?php
 //Functional example to rename an object.
 //Copyright (C) 2013 IsmAvatar <IsmAvatar@gmail.com>
@@ -33,9 +34,91 @@ require_once('cloud/tree.php');
 .filenamecell {
   padding-left: 12px;
 }
+.fileactioncell {
+  padding: 2px;
+  text-align: center;
+  vertical-align: middle;
+  width: 24px;
+  font-size: 20px;
+  padding: 1px !important;
+}
+.filedelcell a {
+  color: #EA0000;
+  text-decoration: none;
+}
+.filemovecell a {
+  color: #A5742A;
+  text-decoration: none;
+}
 .filecrumbspan {
   margin: 6px 8px;
   display: inline-block;
+}
+.fileuploadbox {
+  width: 600px;
+  margin-top: 32px;
+}
+.fileuploadcaption {
+  border: 1px solid navy;
+  background-color: #C0D0FF;
+  text-transform: uppercase;
+  font-weight: bold;
+  padding: 4px;
+  color: navy;
+}
+.fileuploadform {
+  border: 1px solid navy;
+  border-top: none;
+  overflow: auto;
+  padding: 4px;
+}
+.fileuploadform b {
+  font-weight: bold;
+}
+.rawoutputwindow {
+  border: 4px double navy;
+  width: 600px;
+  max-height: 196px;
+  overflow: scroll;
+  box-sizing: border-box;
+}
+.filepagebutton, .filepagebuttondead {
+  width: 18px;
+  height: 18px;
+  border-radius: 10px;
+  text-align: center;
+  vertical-align: middle;
+  display: inline-block;
+  margin: 8px 8px 8px 0;
+}
+.filepagebutton {
+  border: 1px solid navy;
+  background-color: #C0D0FF;
+}
+.filepagebuttondead {
+  border: 1px solid #404040;
+  background-color: #808080;
+  color: #303030;
+}
+.filepagebutton a {
+  color: navy;
+  text-decoration: none;
+}
+.fileopsuccess, .fileopfailure {
+  padding: 4px 12px;
+  border-radius: 8px;
+  max-width: 720px;
+  box-sizing: border-box;
+}
+.fileopsuccess {
+  border: 2px solid #228B22;
+  background-color: #70C060;
+  color: #D0F8B0;
+}
+.fileopfailure {
+  border: 2px solid #832;
+  background-color: #F42;
+  color: #FFE0C0;
 }
 </style>
 <br/>
@@ -60,63 +143,263 @@ if ($prefix == "/") {
   }
 }
 
-function pathfor($p) {
-  $fname = '';
-  return $fname . '?path=' . $p;
+// ============================================================================
+// === Helpers ================================================================
+// ============================================================================
+
+function getName() {
+  return "";
 }
+
+function pageFor($p) {
+  return getName() . '?path=' . $p;
+}
+
+function pageForListBefore($p, $before) {
+  return getName() . '?path=' . $p . '&before=' . $before;
+}
+
+function pageForListAfter($p, $after) {
+  return getName() . '?path=' . $p . '&after=' . $after;
+}
+
+function isDirectory($file) {
+  return property_exists($file, 'subdir');
+}
+
+function prefixOf($file) {
+  return isDirectory($file)? $file->subdir : $file->name;
+}
+
+function delLink($f) {
+  return getName() . "?del=" . prefixOf($f);
+}
+
+function delOnclick($f) {
+  return "return confirmDelete('" . prefixOf($f) . "', "
+      . (isDirectory($f)? 'true' : 'false') . ")";
+}
+
+function moveLink($f) {
+  return "#dontgoanywhere";
+}
+
+function moveOnclick($f)	{
+  return "doMove('" . prefixOf($f) . "')";
+}
+
+function filePath($file) {
+  $ix = strrpos($file, '/');
+  if ($ix === FALSE) {
+    return "";
+  }
+  return substr($file, 0, $ix);
+}
+
+// ============================================================================
+// === Are we doing something special? ========================================
+// ============================================================================
+
+// Handle file uploads
+if (isset($_POST['upload']) && isset($_FILES['uploadfile'])) {
+  $file = $_FILES['uploadfile'];
+  $dest = $_POST['uploaddest'];
+  if (substr($dest, -1) == '/') {
+    $dest .= $file['name'];
+  }
+  $result = putObject($s,$c,$dest,$file,$authHdr);
+  if ($result['success']) {
+    echo '<div class="fileopsuccess">'
+       . 'Successfully uploaded "' . $file['name'] . '" as "' . $dest
+       . '" (' . $result['hash'] . ')<br />on ' . $result['date']
+       . "</div>\n";
+    echo '<br /><a href="?path=' . filePath($dest) . '">Jump to directory</a>';
+  } else {
+    echo '<div class="fileopfailure">Upload failed'
+       . ' (Code ' . $result['code'] . ')<br />' . "\n"
+       . $result['error']
+       . "</div>\n";
+  }
+  exit;
+}
+
+// ============================================================================
+// === JavaScript =============================================================
+// ============================================================================
+
+?>
+<script type="text/javascript">
+  function endsWithC(str, c) {
+    return str[str.length - 1] == c;
+  }
+
+  function confirmDelete(file, isdir) {
+    return confirm((isdir
+        ? (file + " is a directory containing at least one file."
+            + " Are you sure you want to delete it and all its contents?")
+        : "Are you sure you want to delete the file, " + file + "?")
+        + " This cannot be undone.");
+  }
+
+  function doMove(file) {
+    nname = prompt("Please enter the new name of this file.", file);
+    nname = nname? nname.trim() : "";
+    if (nname == "") {
+      return;
+    }
+    
+    var warnings = "";
+    if (endsWithC(file, '/') && !endsWithC(nname, '/')) {
+      warnings += " The new name is not a directory!";
+    }
+    
+    if (confirm("Rename \"" + file + "\" to \"" + nname + "\"?" + warnings)) {
+      window.location.href = "?rename&oldname=" + file + "&newname=" + nname;
+    }
+  }
+</script>
+<?php
 
 // ============================================================================
 // === Breadcrumb =============================================================
 // ============================================================================
 
+echo '<span class="filecrumbspan" style="margin-left: 0; margin-right: 0;">'
+   . '<a href="' . pageFor("") . '">'
+   . '/</a></span>';
+
 if ($prefix != '') {
   $lastslash = 0;
   while (($slash = strpos($prefix, '/', $lastslash)) !== FALSE) {
-    echo '/<span class="filecrumbspan">'
-       . '<a href="' . pathfor(substr($prefix, 0, $slash)) . '">'
+    echo '<span class="filecrumbspan">'
+       . '<a href="' . pageFor(substr($prefix, 0, $slash)) . '">'
        . substr($prefix, $lastslash, $slash - $lastslash)
-       . '</a></span>';
+       . '</a></span>/';
     $lastslash = $slash + 1;
   }
-  echo '<br/>';
+  echo "<br />\n";
 }
 
 // ============================================================================
 // === File list ==============================================================
 // ============================================================================
 
-$files = getObjectsByPath($s,$c,$prefix,$authHdr);
+$limit = isset($_GET['limit'])? intval($_GET['limit']) : 10;
+$limits = Array("limit" => $limit + 1);
+$limitBelow = $limitAbove = FALSE;
+if (isset($_GET['after'])) {
+  $limits['lower'] = $limitBelow = $_GET['after'];
+}
+if (isset($_GET['before'])) {
+  $limits['before'] = $limitAbove = $_GET['before'];
+}
+$files = getObjectsByPath($s,$c,$prefix,$authHdr,$limits);
+echo "<form method=\"post\">\n";
 echo "  <table class=\"filetable\"><tbody>\n";
 
 echo '    <tr><th><input type="checkbox" id="file-checkall" /></th>';
-echo '<th>Path/filename</th></tr>';
+echo '<th>Path/filename</th><th></th><th></th></tr>';
 
+$nextpage = FALSE;
+$count = 0;
+$lastItem = FALSE;
+$firstItem = FALSE;
 foreach ($files as $key => $file) {
-  if (property_exists($file, 'subdir')) {
+  $isDir = isDirectory($file);
+  if ($isDir) {
     $name = htmlspecialchars($file->subdir);
-    $href = pathfor($name);
+    $href = pageFor($name);
   } else {
     $name = $file->name;
     $href = $download_root . $file->name;
   }
+
+  if ($firstItem === FALSE) {
+    $firstItem = $name;
+  }
+
+  if (++$count > $limit) {
+    $nextpage = TRUE;
+    break;
+  }
+
+  $lastItem = $name;
   echo '    <tr><td class="filecheckbox">'
      . '<input type="checkbox" name="sel-' . $key . '" /></td>';
   echo '<td class="filenamecell"><a href="' . htmlspecialchars($href) . '">'
      . htmlspecialchars($name)
-     . '</a>';
-  echo "</td></tr>\n";
+     . '</a></td>';
+  echo '<td class="fileactioncell filemovecell"><a href="' . moveLink($file)
+     . '" onclick="' . moveOnclick($file) . '">✎</td>';
+  echo '<td class="fileactioncell filedelcell"><a href="' . delLink($file)
+     . '" onclick="' . delOnclick($file) . '">✗</td>';
+  echo "</tr>\n";
 }
 
 $lp = strrpos($prefix, '/', -2);
+if ($lp === FALSE && $prefix != "") {
+  $lp = 0;
+}
 if ($lp !== FALSE) {
-  echo '    <tr><td class="filecheckbox">&nbsp;</td><td>'
-     . '<a href="' . pathfor(substr($prefix, 0, $lp)) . '">../</a>'
-     . '</td></tr>';
+  echo '    <tr><td class="filecheckbox">&nbsp;</td><td class="filenamecell">'
+     . '<a href="' . pageFor(substr($prefix, 0, $lp)) . '">../</a>'
+     . "</td><td></td><td></td></tr>\n";
 }
 
 echo '  </tbody></table>';
+echo '</form>';
+
+// ============================================================================
+// === Page controls ==========================================================
+// ============================================================================
+
+if ($limitBelow !== FALSE || ($nextpage && $limitAbove !== FALSE)) {
+  echo '<div class="filepagebutton">'
+     . '<a href="' . pageFor($prefix) .'">◂◂</a>'
+     . '</div>';
+} else {
+  echo '<div class="filepagebuttondead">◂◂</div>';
+}
+
+if ($limitBelow !== FALSE || ($nextpage && $limitAbove !== FALSE)) {
+  echo '<div class="filepagebutton">'
+     . '<a href="' . pageForListBefore($prefix, $firstItem) .'">◀</a>'
+     . '</div>';
+} else {
+  echo '<div class="filepagebuttondead">◀</div>';
+}
+
+if ($nextpage && $lastItem !== FALSE) {
+  echo '<div class="filepagebutton">'
+     . '<a href="' . pageForListAfter($prefix, $lastItem) .'">▶</a>'
+     . '</div>';
+} else {
+  echo '<div class="filepagebuttondead">▶</div>';
+}
+
+// ============================================================================
+// === Upload form ============================================================
+// ============================================================================
+
+?>
+<div class="fileuploadbox">
+  <div class="fileuploadcaption">Upload File</div>
+  <div class="fileuploadform">
+    <form method="post" enctype="multipart/form-data">
+      <div style="float:left">
+        <input type="hidden" name="upload" />
+        <input type="file" name="uploadfile" /><br />
+        <b>Destination</b>: <input type="text" name="uploaddest" value="<?php
+           echo htmlspecialchars($prefix);
+        ?>" />
+      </div>
+      <input type="submit" value="Upload" style="float: right; height: 48px;" />
+    </form>
+  </div>
+</div>
+<?php
 
 echo '<br/><br/><br/>';
-echo "<pre>"; print_r($files); echo "</pre>";
+echo "<pre class=\"rawoutputwindow\">"; print_r($files); echo "</pre>";
 
 ?>
